19cd8408f5e2a5742e38fd0d8d5efbd7f7543e52
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2023-08-19 17:28:00 +0300

    Implement IRQ and NMI

--- Emulator/CPU/CPU.Methods.cs
+++ Emulator/CPU/CPU.Methods.cs
@@ -1,4 +1,4 @@
-using Utilities;
+﻿using Utilities;
 
 namespace Emulator;
 
@@ -38,8 +38,59 @@ public partial class CPU {
 
     public bool IsInstructionComplete() => this.m_Cycles == 0;
 
-    public void InterruptRequest() => NotImplemented("CPU.InterruptRequest");
-    public void NonMaskableInterrupt() => NotImplemented("CPU.NonMaskableInterrupt");
+    public void InterruptRequest() {
+        if (this.Status.InterruptDisable) return;
+
+        // Push the program counter to the stack
+        this.Write((uint16_t)(this.SP + 0x0100), (uint8_t)((this.PC >> 8) & 0x00FF));
+        this.SP--;
+        this.Write((uint16_t)(this.SP + 0x0100), (uint8_t)(this.PC & 0x00FF));
+        this.SP--;
+
+        // Push the status register to the stack
+        this.Status.BreakCommand = false;
+        this.Status.Unused = true;
+        this.Status.InterruptDisable = true;
+        this.Write((uint16_t)(this.SP + 0x0100), this.Status.Data);
+        this.SP--;
+
+        // Set the program counter to the address at 0xFFFE
+        this.m_AbsoluteAddress = 0xFFFE;
+        uint16_t lowByte = this.Read(this.m_AbsoluteAddress);
+        uint16_t highByte = this.Read((uint16_t)(this.m_AbsoluteAddress + 1));
+        this.PC = (uint16_t)((highByte << 8) | lowByte);
+
+        // IRQs take 7 cycles
+        this.m_Cycles = 7;
+    }
+
+    public void NonMaskableInterrupt() {
+        // This is the same as an IRQ except it uses a different interrupt vector address
+        // and it cannot be disabled by setting the interrupt disable flag. The NMI takes
+        // 8 cycles to complete.
+
+        // Push the program counter to the stack
+        this.Write((uint16_t)(this.SP + 0x0100), (uint8_t)((this.PC >> 8) & 0x00FF));
+        this.SP--;
+        this.Write((uint16_t)(this.SP + 0x0100), (uint8_t)(this.PC & 0x00FF));
+        this.SP--;
+
+        // Push the status register to the stack
+        this.Status.BreakCommand = false;
+        this.Status.Unused = true;
+        this.Status.InterruptDisable = true;
+        this.Write((uint16_t)(this.SP + 0x0100), this.Status.Data);
+        this.SP--;
+
+        // Set the program counter to the address at 0xFFFA
+        this.m_AbsoluteAddress = 0xFFFA;
+        uint16_t lowByte = this.Read(this.m_AbsoluteAddress);
+        uint16_t highByte = this.Read((uint16_t)(this.m_AbsoluteAddress + 1));
+        this.PC = (uint16_t)((highByte << 8) | lowByte);
+
+        // NMIs take 8 cycles
+        this.m_Cycles = 8;
+    }
 
     public unsafe void Clock() {
         if (this.m_Cycles == 0) {
